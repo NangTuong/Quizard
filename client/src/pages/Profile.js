8c1dcbaa4231218c5782d33bdf6d9e5a067c4ef3
Connor Allen <m.connor.allen@gmail.com>
2022-09-18 15:56:38 -0400

    merge

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -5,6 +5,12 @@ function Profile() {
     return (
         <div className="container">
             <h1>Name placeholder</h1>
+            <h2></h2>
+            <div>
+                Loop over the quizzes here when backend is set up -CA
+            </div>
         </div>
     );
 }
+
+export default Profile;
