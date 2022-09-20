19c99ad116d32f09292ae9692b0abc79a802c69c
Connor Allen <m.connor.allen@gmail.com>
2022-09-20 19:17:16 -0400

    quiz list object is loaded in component

--- client/src/components/MyQuiz/index.js
+++ client/src/components/MyQuiz/index.js
@@ -1,18 +1,33 @@
 import React from 'react';
-import { QUERY_ME } from '../../utils/queries';
+import { QUERY_USER, QUERY_ME } from '../../utils/queries';
 import { useQuery } from '@apollo/client';
+import { useParams } from 'react-router-dom';
+
 
 const MyQuiz = () => {
-    const  {loading, data } = useQuery(QUERY_ME, {});
+    const { username: userParam } = useParams();
 
-    if (loading) {
-        return <div>Loading...</div>
+    const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+      variables: {username: userParam }
+   });
+
+   const user = data?.me || data?.user || {};
+   if (user.username) {
+     console.log(user);
     }
-    console.log(data)
-
 
     return (
+        
         <div>
+            <h2>{userParam ? `${user.username}'s` : 'Your'} Quizzes</h2>
+            {user.quizzes.map((quiz) => (
+                <div key={quiz._id}>
+                    <div>
+                    
+                    </div>
+                </div>
+            ))}
+            
         </div>
     )
 }
